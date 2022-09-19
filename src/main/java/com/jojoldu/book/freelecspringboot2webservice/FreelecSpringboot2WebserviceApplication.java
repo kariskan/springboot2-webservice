b5ec3bc53ac97ba9c97f07ef4bdd806176f0db08
kariskan <zkfzpf56@naver.com>
2022-09-19 16:40:12 +0900

    H2, JPA setting

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
@@ -2,8 +2,10 @@ package com.jojoldu.book.freelecspringboot2webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //JPA Auditing 기능 활성화
 public class FreelecSpringboot2WebserviceApplication {
 
 	public static void main(String[] args) {
