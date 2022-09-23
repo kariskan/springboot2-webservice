f8bd38c7bccc8850348550a91af90058d851645c
kariskan <zkfzpf56@naver.com>
2022-09-23 15:00:46 +0900

    Screen layout via Mustache

--- src/main/java/com/kariskan/practice/webservice/service/poasts/PostsService.java
+++ src/main/java/com/kariskan/practice/webservice/service/poasts/PostsService.java
@@ -2,9 +2,12 @@ package com.kariskan.practice.webservice.service.poasts;
 
 import com.kariskan.practice.webservice.domain.posts.Posts;
 import com.kariskan.practice.webservice.domain.posts.PostsRepository;
+import com.kariskan.practice.webservice.web.dto.PostsListResponseDto;
 import com.kariskan.practice.webservice.web.dto.PostsResponseDto;
 import com.kariskan.practice.webservice.web.dto.PostsSaveRequestDto;
 import com.kariskan.practice.webservice.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -34,5 +37,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(post);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
